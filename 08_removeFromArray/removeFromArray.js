d91d3ac25632efa6cfaafd721dc7fb7d80331b6e
Znojus <150673971+Znojus@users.noreply.github.com>
2025-08-22 17:12:59 +0300

    Solve 08 exercise of JS

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for(num of args) {
+        for(let i = 0; i < array.length; i++) {
+            if (array[i] === num) {
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
