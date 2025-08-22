d4a2cbfa8813c496a58ae00bf2d1f1a6908a7e82
Znojus <150673971+Znojus@users.noreply.github.com>
2025-08-22 17:30:40 +0300

    solve 09 exercise of JS

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
 
+    if(!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return "ERROR";
+    }
+
+    if (a > b) {
+        for(let i = b; i <= a; i++) {
+            sum += i;
+        }
+    }
+    else if (a < b) {
+        for (let i = a; i <= b; i++) {
+            sum += i;
+        }
+    }
+    else {
+        sum = a;
+    }
+
+    return sum;
 };
+
+//console.log(sumAll(9, 2.8));
 
 // Do not edit below this line
 module.exports = sumAll;
